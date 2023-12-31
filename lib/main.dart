ab375850558433c7736a3d22227a9e783eeff535
August Comstock <augustcomstock@Augusts-MBP.lan>
2023-12-31 13:20:10 -0800

    changed server variable to try catch statement

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,34 @@
 import 'dart:async';
 import 'dart:core';
+import 'package:rpi_gpio/gpio.dart';
 import 'package:rpi_gpio/rpi_gpio.dart';
 import 'functions/get_date_time.dart';
 import 'functions/output_app.dart';
-
-final isServerEnvironment = false;
 
 final scheduledTimeInt = 7;
 final scheduledWeekdaysIntList = [1, 2, 3, 4, 5];
 
 void main() async {
-  if (isServerEnvironment) {
+  try {
+    final gpio = await initialize_RpiGpio();
     Timer.periodic(Duration(minutes: 1), (Timer t) async {
       if (checkDayAndTimeMatch(
           scheduledTimeInt: scheduledTimeInt,
           scheduledWeekdaysIntList: scheduledWeekdaysIntList)) {
-        final gpio = await initialize_RpiGpio();
         await unlockLock(gpio);
         print('It is time to open the door!');
       } else {
         print('It is not time to open the door yet.');
       }
     });
-  } else {
+  } on GpioException catch (e) {
+    print(e);
     print(DateTime.now().weekday);
     print(DateTime.now().hour);
     print(
         "time check: ${checkDayAndTimeMatch(scheduledTimeInt: scheduledTimeInt, scheduledWeekdaysIntList: scheduledWeekdaysIntList)}");
     print('It is time to open the door!');
+  } catch (e) {
+    print('Unexpected error: $e');
   }
 }
